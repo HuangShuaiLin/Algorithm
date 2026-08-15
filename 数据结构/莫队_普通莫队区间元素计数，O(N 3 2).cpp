8161c191b2f00ad398297a7d787c92f9a87bd267
HuangShuaiLin <3532558124@qq.com>
2026-08-15 14:41:38 +0800

    chore: 整理根目录代码分类与命名 - 删除44个与 xl's mumumumuban 内容完全重复的文件（MD5校验一致） - 27个变体文件统一润色命名：英文名_中文注释，O(复杂度).cpp - 16个散落 .cpp 归入对应分类：图论/字符串/数学/数据结构/其他 - 根目录无散落代码文件，结构清晰

---
+++ 数据结构/莫队_普通莫队区间元素计数，O(N 3 2).cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MAX = 1000000;
+int n,m;
+bool compare(const tuple<int,int,int,int> &p1,const tuple<int,int,int,int> &p2){//������˵���˳���������򣬰��Ҷ˵�����
+    auto [l,r,s,id1] = p1;
+    auto [u,v,w,id2] = p2;
+    if(s!=w){
+        return s<w;
+    }
+    return r<v;
+}
+array<int,MAX> cnt = {0};
+void solve(){
+    vector<int> a(n+1);
+    vector<int> b(n+1);
+    unordered_map<int,int> mp;
+    vector<tuple<int,int,int,int> > pq(m+1);//���ѯ���������
+    for(int i=1;i<=n;i++){
+        cin>>a[i];    
+    }
+    for(int i=1;i<=m;i++){
+        int u,v;
+        cin>>u>>v;
+        pq[i] = {u,v,0,i};
+    }
+    int len = sqrt(n);//��arr���ȷ�Ϊ����n�Ŀ�
+    for(int i=0;i*len<=n;i++){
+        for(int j=len*i+1,cnt=0;cnt<len&&j<=n;j++,cnt++){
+            d[j] = i+1;
+        }
+    }
+    for(int i=1;i<=m;i++){
+        auto &[l,r,s,w] = pq[i];
+        s = d[l];
+    }
+    vector<bool> ans()
+    sort(pq.begin()+1,pq.end(),compare);
+    auto [left,right,s,id1] = pq[1];
+    for(int i=left;i<=right;i++){//ά����һ������
+        cnt[a[i]]++;
+    }
+
+    for(int i=1;i<=m;i++){//��ʼ�������䴰��
+        auto [l,r,w,id2] = pq[i];
+        while(left<l){
+            cnt[a[left]]--;
+            left++;
+        }
+        while(left>l){
+            left--;
+            cnt[a[left]]++;
+        }
+        while(right>r){
+            cnt[a[right]]--;
+            right--;
+        }
+        while(right<r){
+            right++;
+            cnt[a[right]]++;
+        }
+    }
+}   
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        cin>>n>>m;
+        solve();
+    }
+}
